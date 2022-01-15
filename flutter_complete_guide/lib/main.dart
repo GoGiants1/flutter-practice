848de9ef980c22e2a0c1d6536a577b7fe0e4459b
GoGiants1 <chooi221@snu.ac.kr>
2022-01-15 21:43:00 +0900

    Add Raised Btns

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -13,12 +13,38 @@ class MyApp extends StatelessWidget {
   // methods
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What\'s your favorite color?",
+      'What\'s your favorite animal',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        body: Text("THis is my default Text!"),
+        // 어떻게 body에 1개 이상의 위젯을 넘길까? => Use container Widget
+        // Column: vertical direction container
+        body: Column(children: [
+          Text('The Question!'),
+          // Deprecated but stick to it in this Project
+          RaisedButton(
+            child: Text("Answer 1"),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text("Answer 2"),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text("Answer 3"),
+            onPressed: null,
+          ),
+          // alt of RaiseBtn
+          ElevatedButton(
+            onPressed: null,
+            child: Text("Elv Btn"),
+          )
+        ]),
       ),
     );
   }
