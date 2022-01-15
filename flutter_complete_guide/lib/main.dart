5662b770dd151116c586a4ae492477accfe1dcbc
GoGiants1 <chooi221@snu.ac.kr>
2022-01-15 21:32:12 +0900

    Add Material UI Scaffold Widget

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   // Instanciationg MyApp()
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // properties
 
   // methods
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello!"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Text("THis is my default Text!"),
+      ),
     );
   }
 }
